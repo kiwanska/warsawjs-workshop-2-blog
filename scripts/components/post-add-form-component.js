7077e95b4d54ab6818dc3e9a1a1cf497b32f0dd1
Kasia Iwańska <sobylmine@gmail.com>
2017-01-15 11:48:29 +0100

    Initial files with mustache

---
+++ scripts/components/post-add-form-component.js
@@ -0,0 +1,13 @@
+class postAddFormComponent {
+
+    constructor() {
+        this.template = document.getElementById('post-add-form-component').innerHTML;
+        this.$container = document.querySelector('.container');
+    }
+
+    render () {
+        // this.container.appendChild(this.template);
+    }
+
+}
+
---
+++ scripts/components/post-component.js
@@ -0,0 +1,11 @@
+class postComponent {
+
+    constructor() {
+        this.template = document.getElementById('post-component').innerHTML;
+        this.$container = document.querySelector('.container');
+    }
+
+    render () {
+        // this.container.innerHTML = this.template;
+    }
+}
